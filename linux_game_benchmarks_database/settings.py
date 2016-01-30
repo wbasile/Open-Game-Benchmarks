502cea0b72180af74f7f56bd04fcb5c5dfb4d8e4
Walter Basile <wbasile80@gmail.com>
2016-01-30 14:43:14 +0100

    added lines to fps chart

--- linux_game_benchmarks_database/settings.py
+++ linux_game_benchmarks_database/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "zesxj^#0k&7q4$5+k562y7#_#@eoz8ae)(8o(-+k^iq&ecm!l9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
